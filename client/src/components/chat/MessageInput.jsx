dab67d955d871fd7da14aadcc4206b22606b7966
lakshya0310 <lakshya.jindal@research.iiit.ac.in>
2026-07-04 18:52:08 +0530

    feat(chat) add emoji picker

--- client/src/components/chat/MessageInput.jsx
+++ client/src/components/chat/MessageInput.jsx
@@ -1,8 +1,11 @@
 import { useState, useRef } from "react";
+import EmojiPicker from "emoji-picker-react";
 
 function MessageInput({ onSend, onTyping, onStopTyping }) {
 
     const [text, setText] = useState("");
+    const [showPicker, setShowPicker] = useState(false);
+
     const timeoutRef = useRef(null);
 
     const handleChange = (e) => {
@@ -34,29 +37,73 @@ function MessageInput({ onSend, onTyping, onStopTyping }) {
         onStopTyping();
 
     };
+    const handleEmojiClick = (emojiData) => {
 
+    setText((prev) => prev + emojiData.emoji);
+
+    onTyping();
+
+    clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = setTimeout(() => {
+        onStopTyping();
+    }, 1000);
+
+    setShowPicker(false);
+
+};
     return (
 
-        <form
-            onSubmit={handleSubmit}
-            className="border-t bg-white p-4 flex"
+        <div className="relative border-t bg-white p-4">
+
+    {
+
+        showPicker && (
+
+            <div className="absolute bottom-20 left-4">
+
+                <EmojiPicker
+                    onEmojiClick={handleEmojiClick}
+                />
+
+            </div>
+
+        )
+
+    }
+
+    <form
+        onSubmit={handleSubmit}
+        className="flex"
+    >
+
+        <button
+            type="button"
+            onClick={() =>
+                setShowPicker(!showPicker)
+            }
+            className="mr-2 text-2xl"
         >
+            😊
+        </button>
 
-            <input
-                value={text}
-                onChange={handleChange}
-                placeholder="Type a message..."
-                className="flex-1 border rounded px-4 py-2"
-            />
+        <input
+            value={text}
+            onChange={handleChange}
+            placeholder="Type a message..."
+            className="flex-1 border rounded px-4 py-2"
+        />
 
-            <button
-                type="submit"
-                className="ml-3 bg-blue-600 text-white px-6 rounded"
-            >
-                Send
-            </button>
+        <button
+            type="submit"
+            className="ml-3 bg-blue-600 text-white px-6 rounded"
+        >
+            Send
+        </button>
 
-        </form>
+    </form>
+
+</div>
 
     );
 
